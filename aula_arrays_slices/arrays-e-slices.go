f3eaf529a2b76d79c705941282f52410aec06630
Jeff <guinho.psp1@gmail.com>
2025-03-07 11:22:58 -0300

    estudo sobre arrays e slices

---
+++ aula_arrays_slices/arrays-e-slices.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("arrays e slices")
+	//arrays
+	var array1 [5]int
+	array1[0] = 2
+	fmt.Println(array1)
+
+	arrayStr := [4]string{"jeff", "ana", "vi", "jorge"}
+	fmt.Println(arrayStr)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	//slices
+	//nao precisa limitar o tamanho
+	slice := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(slice)
+
+	fmt.Println("verifica os tipos")
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array1))
+	//adicionar
+	slice = append(slice, 34)
+
+	fmt.Println(slice)
+
+	// na sintaxe de [n...m] n= inclusivo e m= exclusivo
+	// ponteiro para o arrayStr
+	slice2 := arrayStr[1:4]
+	fmt.Println(slice2)
+
+}
