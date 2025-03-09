df6a2ba57425abe3fa71c54b4e029f4d1341fd15
Jeff <guinho.psp1@gmail.com>
2025-03-09 17:29:04 -0300

    subindo app cli

--- app_linha_comando/app/app.go
+++ app_linha_comando/app/app.go
@@ -14,17 +14,26 @@ func Gerar() *cli.App {
 	app.Name = "app de linha de comando"
 	app.Usage = "busca ip e nome de servires na internet"
 
+	flags :=
+		[]cli.Flag{
+			cli.StringFlag{
+				Name:  "host",
+				Value: "devbook.com.br",
+			},
+		}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de enderecos na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de enderecos na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidor",
+			Usage:  "busca servidores",
+			Flags:  flags,
+			Action: buscaServidores,
 		},
 	}
 	return app
@@ -42,5 +51,20 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscaServidores(c *cli.Context) {
+	host := c.String("host")
+
+	//serv
+	servidores, erro := net.LookupNS(host) // name service = nome do servidor
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
